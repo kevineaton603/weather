f72b26804a0bbd67a6d73a3843be26d098229c35
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2020-04-20 22:45:50 -0400

    push for class

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'Weather')}}</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <h1>About</h1>
-    </body>
-</html>
-
-<!-- 
 @extends('layouts.app')
 @section('content')
     <h1>About</h1>
@@ -24,4 +9,3 @@
         @endif
     </ul>
 @endsection
--->
